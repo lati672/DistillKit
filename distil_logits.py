093b594923e46756378ede397b511a8f424430f5
lati672 <1297091773@qq.com>
2025-04-10 10:12:13 +1000

    reduced the num_samples

--- distil_logits.py
+++ distil_logits.py
@@ -13,7 +13,7 @@ config = {
     "dataset": {
         "name": "mlabonne/FineTome-100k",
         "split": "train",
-        # "num_samples": , # You can pass a number here to limit the number of samples to use.
+        "num_samples": 5000, # You can pass a number here to limit the number of samples to use.
         "seed": 42
     },
     "models": {
